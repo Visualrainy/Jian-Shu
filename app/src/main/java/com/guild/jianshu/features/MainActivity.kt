8a85acd77b4bfdb419d942e809afc70c14c95d87
Peng <Visualrainy@gmail.com>
2018-03-19 22:19:23 +0800

    add bottom tab by adding NavigationBottomView

--- app/src/main/java/com/guild/jianshu/features/MainActivity.kt
+++ app/src/main/java/com/guild/jianshu/features/MainActivity.kt
@@ -2,12 +2,41 @@ package com.guild.jianshu.features
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.guild.jianshu.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigationView()
+    }
+
+    private fun initNavigationView() {
+        bottom_navigation.setOnNavigationItemSelectedListener({item ->
+            when(item.itemId) {
+                R.id.recent -> {
+                    Log.d(LOG_TAG, "click recent")
+                }
+                R.id.discovery -> {
+                    Log.d(LOG_TAG, "click discovery")
+                }
+                R.id.publish -> {
+                    Log.d(LOG_TAG, "click publish")
+                }
+                R.id.notification -> {
+                    Log.d(LOG_TAG, "click notification")
+                }
+                R.id.profile -> {
+                    Log.d(LOG_TAG, "click profile")
+                }
+            }
+            true
+        })
     }
 }
