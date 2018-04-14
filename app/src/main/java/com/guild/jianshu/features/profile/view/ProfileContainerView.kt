adee6d80358479ce36cb5394ce59f6bbd69b5580
Peng <Visualrainy@gmail.com>
2018-04-14 23:08:23 +0800

    using custom layout replace of LinearLayout and fix the constructor function issue when using custom layout in xml

--- app/src/main/java/com/guild/jianshu/features/profile/view/ProfileContainerView.kt
+++ app/src/main/java/com/guild/jianshu/features/profile/view/ProfileContainerView.kt
@@ -1,12 +1,17 @@
 package com.guild.jianshu.features.profile.view
 
 import android.content.Context
+import android.util.AttributeSet
 import android.widget.LinearLayout
 import com.guild.jianshu.features.profile.ProfileContainerAdapter
 
-class ProfileContainerView(context: Context) : LinearLayout(context) {
+class ProfileContainerView : LinearLayout {
 
     private var profileContainerAdapter: ProfileContainerAdapter? = null
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     fun setAdapter(adapter: ProfileContainerAdapter) {
         profileContainerAdapter = adapter
